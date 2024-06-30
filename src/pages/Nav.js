f42b47fd61d72ef5af97ab15a90a8eff1d3a99ba
AYOUB134 <113133550+AYOUB134@users.noreply.github.com>
2024-06-30 19:05:56 +0500

    Update Nav.js

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -3,12 +3,16 @@ import React from 'react';
 const Nav = () => {
     return (
         <nav className="bg-blue-600 p-4 shadow-md">
-            <div className="container mx-auto flex justify-center items-center">
-                <div className="text-white text-2xl sm:text-5xl md:text-6xl lg:text-4xl xl:text-6xl font-bold">
-                    <span className="text-green-200">City Medical</span>
-                    <span className="text-yellow-300">  & Cerjical</span>
-                    <span className="text-pink-200"> Complex</span>
-                    <span className="text-blue-200"> Jatoi</span>
+            <div className="container mx-auto flex justify-center items-center text-center">
+                <div className="text-white text-2xl sm:text-3xl md:text-5xl lg:text-4xl xl:text-6xl font-bold">
+                    <div className="sm:block">
+                        <span className="text-green-200">City Medical</span>
+                    </div>
+                    <div className="sm:block">
+                        <span className="text-yellow-300">& Cerjical</span>
+                        <span className="text-pink-200"> Complex</span>
+                        <span className="text-blue-200"> Jatoi</span>
+                    </div>
                 </div>
             </div>
         </nav>
