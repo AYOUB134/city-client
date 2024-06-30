f05816c2a5cea29dae8767d68d20b309321d5b1c
AYOUB134 <113133550+AYOUB134@users.noreply.github.com>
2024-06-30 19:05:13 +0500

    Update Patients.js

--- src/features/patients/Patients.js
+++ src/features/patients/Patients.js
@@ -35,8 +35,17 @@ const Patients = () => {
   const isCurrentWeek = (dateTime) => {
     const admissionDate = new Date(dateTime);
     const today = new Date();
-    const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay())); // Sunday
-    const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 6)); // Saturday
+
+    // Get the start of the current week (Sunday)
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(today.getDate() - today.getDay());
+    startOfWeek.setHours(0, 0, 0, 0);
+
+    // Get the end of the current week (Saturday)
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6);
+    endOfWeek.setHours(23, 59, 59, 999);
+
     return admissionDate >= startOfWeek && admissionDate <= endOfWeek;
   };
 
@@ -269,10 +278,6 @@ const Patients = () => {
                 disabled={editingPatient.status === 'out'}
               />
             </label>
-
-
-          
-
             <label className="block mb-2">
               To Dr:
               <input
@@ -285,8 +290,6 @@ const Patients = () => {
                 disabled={editingPatient.status === 'out'}
               />
             </label>
-
-
             <label className="block mb-2">
               Contact Number:
               <input
