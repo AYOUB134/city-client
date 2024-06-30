a6daf97950da4fb7a4323a8a6953d56a9b589654
AYOUB134 <113133550+AYOUB134@users.noreply.github.com>
2024-06-30 14:57:03 +0500

    Update HeroBoxAPI.js

--- src/features/herobox/HeroBoxAPI.js
+++ src/features/herobox/HeroBoxAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api/v1';
+const BASE_URL = 'http://city-server-production.up.railway.app/api/v1';
 
 export const fetchPatients = async () => {
   const response = await fetch(`${BASE_URL}/patients`);
