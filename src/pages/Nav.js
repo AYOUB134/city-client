c1a3ff9e95b05d29b5d8d3ca1576d50181ecfe73
AYOUB134 <113133550+AYOUB134@users.noreply.github.com>
2024-06-30 20:13:09 +0500

    Update Nav.js

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -3,15 +3,19 @@ import React from 'react';
 const Nav = () => {
     return (
         <nav className="bg-blue-600 p-4 shadow-md">
-            <div className="container mx-auto flex justify-center items-center text-center">
-                <div className="text-white text-2xl sm:text-3xl md:text-5xl lg:text-4xl xl:text-6xl font-bold">
-                    <div className="sm:block">
+            <div className="container mx-auto text-center">
+                <div className="text-white font-bold">
+                    <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
                         <span className="text-green-200">City Medical</span>
-                    </div>
-                    <div className="sm:block">
-                        <span className="text-yellow-300">& Cerjical</span>
+                        <span className="text-yellow-300"> & Cerjical</span>
                         <span className="text-pink-200"> Complex</span>
                         <span className="text-blue-200"> Jatoi</span>
+                    </div>
+                    <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-2 font-normal">
+                        <span className="text-green-200">C.E.O</span>
+                        <span className="text-yellow-300"> DR</span>
+                        <span className="text-pink-200"> ZAFFAR</span>
+                        <span className="text-blue-200"> ABBAS SITHARI</span>
                     </div>
                 </div>
             </div>
@@ -20,3 +24,4 @@ const Nav = () => {
 }
 
 export default Nav;
+
